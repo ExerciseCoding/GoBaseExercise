dbcac02b78382bdd33d8f2854ba565758ab67db0
SunshineDay <924345620@qq.com>
2021-05-07 11:34:25 +0800

    os/exec exercise

---
+++ os_exec/demo1/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os/exec"
+)
+
+func main(){
+	var(
+		cmd *exec.Cmd
+		output []byte
+		err error
+	)
+	//生成cmd
+	cmd = exec.Command("/bin/bash", "-c", "ls -l | wc -l")
+
+	//执行命令，捕获子进程的输出
+
+	if output, err = cmd.CombinedOutput(); err != nil {
+		log.Fatalf("exec command fail",err)
+	}
+
+	//打印子进程输出
+
+	fmt.Println(output)
+
+}
